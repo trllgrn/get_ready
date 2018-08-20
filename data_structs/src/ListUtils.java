bf6228ef813cd6b3ff9ec5c19fe94216fd8e8209
trllgrn <trllgrn@gmail.com>
2018-08-20 16:07:14 -0700

    added length to ListUtils

--- data_structs/src/ListUtils.java
+++ data_structs/src/ListUtils.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.LinkedList;
 
 public class ListUtils {
+    public static int size(ListNode<Integer> list) {
+        int count = 0;
+        ListNode<Integer> nodePtr = list;
+        while (nodePtr != null) {
+            count++;
+            nodePtr = nodePtr.next;
+        }
+        return count;
+    }
 }
