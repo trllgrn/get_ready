1f521d06ca1ee07602e7f855a0994c9ab5828406
trllgrn <trllgrn@gmail.com>
2018-08-22 11:08:12 -0700

    added first test - success

--- data_structs/linked_lists/ListUtilsTest.java
+++ data_structs/linked_lists/ListUtilsTest.java
@@ -1,9 +1,17 @@
-package linked_lists;
-import org.junit.*;
+package get_ready.data_structs.linked_lists;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 public class ListUtilsTest {
 
-    @org.junit.Test
-    public void size() {
+    @Test
+    public void ListUtilsSizeTest() {
+        ListNode<Integer> a = new ListNode<Integer>(1);
+        ListNode<Integer> b = new ListNode<Integer>(3);
+        ListNode<Integer> c = new ListNode<Integer>(5);
+        a.next = b;
+        b.next = c;
+        assertThat(ListUtils.size(a), is(3));
     }
 }
